e399f77054dc27c84ce0488ef4790403b1c59c09
King Mark Kevin <king.mark.kevin@iisgalvanimi.edu.it>
2020-03-20 10:44:54 +0000

    punto 2

---
+++ correzione-auto/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { HttpClientModule } from '@angular/common/http'
+import { AppComponent } from './app.component';
+import { DrivingListComponent } from './driving-list/driving-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DrivingListComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
